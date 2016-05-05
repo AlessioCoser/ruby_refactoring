1040f56b3456a95eac8d714d6cf6373033b82562
ale.co1989@gmail.com <Pot_Anglon89>
2016-05-05 09:57:28 +0200

    children statement tests

--- test_ruby_refactoring.rb
+++ test_ruby_refactoring.rb
@@ -5,6 +5,7 @@ require "test/unit"
 class RefactoringTest < Test::Unit::TestCase
 
   MOVIE_REGULAR = Movie.new("Avengers", Movie::REGULAR)
+  MOVIE_CHILDRENS = Movie.new("Cars", Movie::CHILDRENS)
 
   def setup
     @customer = Customer.new "Alessio"
@@ -23,6 +24,22 @@ class RefactoringTest < Test::Unit::TestCase
     @customer.add_rental Rental.new(MOVIE_REGULAR, 3)
     assert_equal @customer.statement, rented_statement("Alessio", "Avengers", 3.5, 1)
   end
+
+  def test_rental_children_statement
+    @customer.add_rental Rental.new(MOVIE_CHILDRENS, 2)
+    assert_equal @customer.statement, rented_statement("Alessio", "Cars", 1.5, 1)
+  end
+
+  def test_rental_children_more_than_three_days
+    @customer.add_rental Rental.new(MOVIE_CHILDRENS, 4)
+    assert_equal @customer.statement, rented_statement("Alessio", "Cars", 3.0, 1)
+  end
+
+  def test_rental_children_eight_days
+    @customer.add_rental Rental.new(MOVIE_CHILDRENS, 8)
+    assert_equal @customer.statement, rented_statement("Alessio", "Cars", 9.0, 1)
+  end
+
 
   def rented_statement(customer, movie, amount, frequent_renter_points)
     "Rental Record for #{customer}\n\t#{movie}\t#{amount}\nAmount owed is #{amount}\nYou earned #{frequent_renter_points} frequent renter points"
